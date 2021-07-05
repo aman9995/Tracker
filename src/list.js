987c4eb8c62570d594fa7294cde1ae580bcde614
aman9995 <79044812+aman9995@users.noreply.github.com>
2021-07-05 10:43:37 +0530

    Update list.js

--- src/list.js
+++ src/list.js
@@ -23,18 +23,6 @@ const casesTypeColors = {
   },
 };
 
-export const sortData = (data) => {
-  let sortedData = [...data];
-  sortedData.sort((a, b) => {
-    if (a.cases > b.cases) {
-      return -1;
-    } else {
-      return 1;
-    }
-  });
-  return sortedData;
-};
-
 export const prettyPrintStat = (stat) =>
   stat ? `+${numeral(stat).format("0.0a")}` : "+0";
 
